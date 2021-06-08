31ef2ca1fe502dd94c5d20b5cb8f84e4af0d9c01
vherrera96 <hugo.tmz96@gmail.com>
2021-06-08 02:39:12 -0500

    feat: add method

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,1 +1,8 @@
-# -*- coding: utf-8 -*-
+class SimpleCalculator:
+    def add(self, *args) -> int or float:
+        """Performs the sum from the input.
+
+        Returns:
+            int or float: sum from input.
+        """
+        return sum(args)
