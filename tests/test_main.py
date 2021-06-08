bd81e34615f9b54cfe17c4507902b1b3b6cbe5cd
vherrera96 <hugo.tmz96@gmail.com>
2021-06-08 02:20:57 -0500

    tests: Add three numbers

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,3 +6,10 @@ def test_add_two_numbers():
     calculator = SimpleCalculator()
     result = calculator.add(4, 5)
     assert result == 9, "Add 5 and 4 should be 9!"
+
+
+def test_add_three_numbers():
+    """Tests if the sum of three numbers behave as expected"""
+    calculator = SimpleCalculator()
+    result = calculator.add(4, 5, 6)
+    assert result == 15, "Sum of 4,5 and 6 should be 15"
