4bd3fdd4e1c185a421d53022959dab21764bf659
vherrera96 <hugo.tmz96@gmail.com>
2021-06-08 02:04:55 -0500

    tests: Add two numbers

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from simple_calculator.main import SimpleCalculator
 
+
+def test_add_two_numbers():
+    """Tests if add method behaves as expected"""
+    calculator = SimpleCalculator()
+    result = calculator.add(4, 5)
+    assert result == 9, "Add 5 and 4 should be 9!"
